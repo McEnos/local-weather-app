72284850c2f0c635eaa2d91b7a96233146ebf1e9
Joshua <joshua.odiwuor@turnkeyafrica.com>
2020-08-25 22:38:38 +0300

    Adds unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,11 @@
 import { TestBed, async } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -19,13 +18,14 @@ describe('AppComponent', () => {
   it(`should have as title 'local-weather-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('local-weather-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('local-weather-app app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'LocalCast Wether'
+    );
   });
 });
