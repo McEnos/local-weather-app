a76d383fc064568e0b16daabe6469f7366e72fa8
Joshua <joshua.odiwuor@turnkeyafrica.com>
2020-08-31 13:28:02 +0300

    using asycn to automatically subscribe $ unsubscribe

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 import { ICurrentWeather } from '../interfaces';
+import { Observable } from 'rxjs';
+import { SubSink } from 'subsink';
 import { WeatherService } from '../weather/weather.service';
 
 @Component({
@@ -8,14 +10,17 @@ import { WeatherService } from '../weather/weather.service';
   templateUrl: './current-weather.component.html',
   styleUrls: ['./current-weather.component.css'],
 })
-export class CurrentWeatherComponent implements OnInit {
-  @Input() current: ICurrentWeather;
-  constructor(private weatherService: WeatherService) {}
+export class CurrentWeatherComponent implements OnInit, OnDestroy {
+  private subscriptions = new SubSink();
+  current$: Observable<ICurrentWeather>;
+  constructor(private weatherService: WeatherService) {
+    this.current$ = this.weatherService.currentWeather$;
+  }
 
   ngOnInit(): void {
-    this.weatherService.currentWeather$.subscribe(
+    /*  this.weatherService.currentWeather$.subscribe(
       (data) => (this.current = data)
-    );
+    ); */
   }
 
   getOrdinal(date: number) {
@@ -23,5 +28,9 @@ export class CurrentWeatherComponent implements OnInit {
     return n > 0
       ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10]
       : '';
+  }
+
+  ngOnDestroy(): void {
+    //this.subscriptions.unsubscribe();
   }
 }
